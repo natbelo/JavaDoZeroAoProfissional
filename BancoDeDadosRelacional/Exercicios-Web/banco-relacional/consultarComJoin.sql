be371ba78ba2f245346e2fac72c84050eb0fa427
Natália Belo <nataliabelo.nb@gmail.com>
2022-02-11 22:42:15 -0300

    Tipos de Join

---
+++ BancoDeDadosRelacional/Exercicios-Web/banco-relacional/consultarComJoin.sql
@@ -0,0 +1,14 @@
+select 
+    e.nome as Estado,
+    c.nome as Cidade,
+    regiao as Regiao
+from estados e, cidades c
+where e.id = c.estado_id;
+
+select 
+    e.nome as Estado,
+    c.nome as Cidade,
+    regiao as Regiao
+from estados e 
+inner join cidades c
+on e.id = c.estado_id;
---
+++ BancoDeDadosRelacional/Exercicios-Web/banco-relacional/inserirEmpresas.sql
@@ -0,0 +1,21 @@
+insert into empresas 
+    (nome, cnpj)
+values 
+    ('Bradesco', 50789918000130),
+    ('Vale', 56605106000173),
+    ('Cielo', 61917362000136);
+
+alter table empresas modify cnpj varchar(14);
+
+desc empresas;
+desc prefeitos;
+select * from empresas;
+select * from cidades;
+
+insert into empresas_unidades
+    (empresa_id, cidade_id, sede)
+values
+    (1, 1, 1),
+    (1, 2, 0),
+    (2, 1, 0),
+    (2, 2, 1);
